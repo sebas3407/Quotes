656db82bf4cb4592afef0111f2775b78a5203fe9
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-01-11 19:51:04 +0100

    Working on the UI

--- Quotes/ViewController.swift
+++ Quotes/ViewController.swift
@@ -13,20 +13,15 @@ class ViewController: UIViewController {
     //UI ELEMENTS
     @IBOutlet weak var lbl_quote: UILabel!
     
+    @IBOutlet weak var lbl_author: UILabel!
+    
     // STRUCTS
     var quoteBook: QuoteBook = QuoteBook()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let num: Int = Int.random(in: 0..<quoteBook.quotes.count)
-        lbl_quote.text = quoteBook.quotes[num]
-        DownloadQuote()
-    }
     
-    @IBAction func ShowQuotes(_ sender: Any) {
-        
-        lbl_quote.text = quoteBook.quotes[1]
+        DownloadQuote()
     }
     
     func DownloadQuote()
@@ -48,7 +43,10 @@ class ViewController: UIViewController {
                 let articlesData = try JSONDecoder().decode(DailyQuote.self, from: data)
                 
                 DispatchQueue.main.async {
+                    
                     self.lbl_quote.text = articlesData.contents.quotes[0].quote
+                    
+                    self.lbl_quote.text = articlesData.contents.quotes[0].author
                 }
                 
             } catch let jsonError {
